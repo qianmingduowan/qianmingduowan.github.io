02f5ad5c2d25dc2d154230678f593173c3b28fa1
qianmingduowan <852449118@qq.com>
2023-06-16 09:15:39 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m Ming Qian. My Chinese name is 钱铭.
+- 👀 I’m interested in computer vision and 3d vision
+- 🌱 I’m currently learning to nerf related works
+- 📫 How to reach me mingqian@whu.edu.cn
+- I'm a PhD student in Wuhan University. you can see my [web here](https://qianmingduowan.github.io/mqian.github.io/) .
+
+<!---
+qianmingduowan/qianmingduowan is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
---
+++ make_white.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+# 读取照片
+image = cv2.imread('images/portrait.jpg')
+
+# 获取照片的宽度和高度
+height, width = image.shape[:2]
+
+# 计算圆的半径（取宽度和高度的最小值的一半）
+radius = min(width, height) // 2
+
+# 创建一个黑色的画布，大小与照片相同
+canvas = np.zeros_like(image)
+
+# 计算圆的中心点
+center_x, center_y = width // 2, height // 2
+
+# 在画布上画一个白色的圆形
+cv2.circle(canvas, (center_x, center_y), radius, (255, 255, 255), -1)
+
+# 将照片与画布进行按位与运算，保留圆形内部的原始图像
+result = cv2.bitwise_and(image, canvas)
+
+# 将圆圈外部区域设置为白色
+result[np.where((result == [0, 0, 0]).all(axis=2))] = [255, 255, 255]
+
+
+# 显示结果
+cv2.imwrite('images/portrait1.jpg', result)
